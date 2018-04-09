810452b0ae919f3136fb32a4df9b025b1a7dee50
atkiis <tikuilla@gmail.com>
2018-04-09 10:51:50 +0300

    Minor fixes

--- ReadLinks.py
+++ ReadLinks.py
@@ -1,15 +1,18 @@
+
 import urllib2
+
 url="http://blog.kiiskinen.at"
 page=urllib2.urlopen(url)
 data=page.read().split("</a>")
 tag="<a href=\""
 endtag="\">"
+
 for item in data:
     if "<a href" in item:
         try:
             ind = item.index(tag)
             item=item[ind+len(tag):]
             end=item.index(endtag)
-        except: pass
+        except BaseException: pass
         else:
             print item[:end]
